bb045361d369b46b83b3835b942c8d615c2c1731
Aniketh Girish <anikethgireesh@gmail.com>
2017-09-24 20:03:41 +0530

    Student class created for academic profile data

---
+++ StudentsDiary/src/org/amrita/u4cse16209/StudentsDiary/Student.java
@@ -0,0 +1,55 @@
+package org.amrita.u4cse16209.StudentsDiary;
+
+public class Student {
+	private String studentName;
+    private String studentNo;
+    private String email;
+    private int year;
+
+
+    public Student() {
+        this.studentName = null;
+        this.studentNo = null;
+        this.email = null;
+        this.year = -1;
+    }
+
+    public Student(String nName, String nNum, String nEmail, int nYr) {
+        this.studentName = nName;
+        this.studentNo = nNum;
+        this.email = nEmail;
+        this.year = nYr;
+    }
+
+    public void setStudentName(String newStudentName) {
+        this.studentName = newStudentName;
+    }
+
+    public void setStudentNo(String newStudentNo) {
+        this.studentNo = newStudentNo;
+    }
+
+    public void setEmail(String newEmail) {
+        this.email = newEmail;
+    }
+
+    public void setYear(int newYear) {
+        this.year = newYear;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getStudentNo() {
+        return studentNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
