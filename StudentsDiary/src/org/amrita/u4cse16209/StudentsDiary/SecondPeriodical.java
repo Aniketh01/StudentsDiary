4648d72c66957a32b6cfca42c47873f6d4d14142
Aniketh Girish <anikethgireesh@gmail.com>
2017-10-01 18:26:11 +0530

    Added Second Periodical management class

---
+++ StudentsDiary/src/org/amrita/u4cse16209/StudentsDiary/SecondPeriodical.java
@@ -0,0 +1,35 @@
+package org.amrita.u4cse16209.StudentsDiary;
+
+import java.util.Scanner;
+
+public class SecondPeriodical {
+public double mark;
+	
+	public SecondPeriodical(int mark) {
+		this.mark = mark;
+	}
+	
+	public double getMark() {
+		return mark;
+	}
+	
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
+
+    public void addMark() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the mark");
+        this.mark  = sc.nextDouble();
+    }
+
+    public String toString() {
+        return "Second Periodical: "+ mark;
+    }
+
+    public void Display() {
+        System.out.println(toString());
+    }
+
+
+}
