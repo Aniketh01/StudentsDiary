74f148797247209947cd70266e9ac4ab2cba734b
Aniketh Girish <anikethgireesh@gmail.com>
2017-10-11 15:43:15 +0530

    CGPA classes created

--- StudentsDiary/src/org/amrita/u4cse16209/StudentsDiary/CGPA.java
+++ StudentsDiary/src/org/amrita/u4cse16209/StudentsDiary/CGPA.java
@@ -7,6 +7,33 @@
 
 package org.amrita.u4cse16209.StudentsDiary;
 
-public class CGPA {
+import java.util.ArrayList;
 
+public class CGPA {
+	private double cgpa;
+	
+	public CGPA() {
+		cgpa = 0;
+	}
+	
+	public void SetCgpa(ArrayList<SGPA> sgpaList) {
+		for (SGPA sgpa: sgpaList) {
+			cgpa = sgpa.getSGPA();
+		}
+		cgpa = cgpa / sgpaList.size();
+	}
+	
+	public double getCGPA() {
+		return cgpa;
+	}
+	
+	public String toString() {
+		return 	"CGPA" + getCGPA();
+	}
+	
+	public void Display() {
+		System.out.println(toString());
+	}
 }
+
+
