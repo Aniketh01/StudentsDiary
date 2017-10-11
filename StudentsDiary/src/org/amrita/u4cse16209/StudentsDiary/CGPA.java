14290313d2bb83b5cbd3a881f246deee37785d38
Aniketh Girish <anikethgireesh@gmail.com>
2017-10-11 15:09:54 +0530

    Added documentation for CGPA and SGPA class

--- StudentsDiary/src/org/amrita/u4cse16209/StudentsDiary/CGPA.java
+++ StudentsDiary/src/org/amrita/u4cse16209/StudentsDiary/CGPA.java
@@ -1,3 +1,10 @@
+/**
+ * @author 2017 Aniketh Girish anikethgireesh@gmail.com
+ * 
+ * This class maintains the CGPA of any particular student according to the needs.
+ * 
+ */
+
 package org.amrita.u4cse16209.StudentsDiary;
 
 public class CGPA {
