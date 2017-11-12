02a7cf6d7fc824bc05acfde278318e9ff8675504
Aniketh Girish <anikethgireesh@gmail.com>
2017-11-12 23:02:46 +0530

    Addded the main class. The final commit

--- StudentsDiary/src/org/amrita/u4cse16209/StudentsDiary/Main.java
+++ StudentsDiary/src/org/amrita/u4cse16209/StudentsDiary/Main.java
@@ -15,16 +15,75 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception{
 		
+		int Scount = 0;
+		int ch = 0;
+		System.out.println("Enter the count of students you want to enter");
+		Scanner input = new Scanner(System.in);
+		Scount = input.nextInt();
 		// Main section Student profile.
+	
 		DataBaseManager manager = new DataBaseManager();
+		Student s1[] = new Student[Scount];
+		Dues due = new Dues();
+		Internals internal = new Internals();
+		Hostel hostel = new Hostel();
+		Personal personal = new Personal();
 		
-		manager.serializeWriteData();
-//		manager.serializeReadData();
-		
-		try {
-			Scanner input = new Scanner(System.in);
-		}
+		Events event = new Events();
+		Library lib = new Library();
+		ParentDetails parent = new ParentDetails();
+		Club club = new Club();
 		
 		
+		int b=0;
+	    while(b!=1) {
+	    System.out.println("Enter your choice :\n1.Write files into Student and Display \n2. Enter and display dues \n3.Write and display personal data \n4.Enter and display Hostel details \n5. Details of Events \n6. Enter parents details \n7. Enter library details \n8. Enter club details \n9. Exit");
+	    ch=input.nextInt();
+	    
+	    switch(ch) {
+	    case 1: for (int i = 0; i < Scount; i++) {
+	    			manager.serializeWriteData();
+	    		}
+	    break;
+	    case 2: for(int k=0; k < Scount; k++) {
+	    			due.newDue();
+	    			due.Display();
+	    		}
+	    break;
+	    case 3: for (int j = 0; j < Scount; j++) {
+	    			personal.getPersonalData();
+	    			personal.writeToFile();
+	    			personal.readFile();
+	    }
+	    break;	    
+	    case 4: for (int c = 0; c < Scount; c++) {
+	    			hostel.addRecords();
+	    			hostel.Display();
+	    }
+	    break;
+	    case 5: for(int a = 0; a < Scount; a++) {
+	    	event.addEvent();
+	    	event.Display();
+	    }
+	    	break;
+	    case 6: for(int x = 0; x < Scount; x++) {
+	    	lib.addFine();
+	    	lib.getFine();
+	    	lib.toString();
+	    }
+	    	break;
+	    case 7: for(int y = 0; y < Scount; y++) {
+	    	parent.getParentDetails();
+	    	parent.displayData();
+	    }
+	    	break;
+	    case 8: for(int y = 0; y < Scount; y++) {
+	    	club.addDetail();
+	    	club.Display();
+	    }
+	    	break;
+	    case 9: return;		
+	}
+	    }
 	}
 }
